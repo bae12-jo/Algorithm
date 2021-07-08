c3aa8f0325736398becd702a0c3d22f0deeecac8
Bae12 <bae12.jo@gmail.com>
2021-07-08 23:21:12 +0900

    210708 tree

--- BOJ/DataStructure/2250.cpp
+++ BOJ/DataStructure/2250.cpp
@@ -29,34 +29,34 @@ int main() {
     a[x].right = z;
     if (y != -1) cnt[y] += 1;
     if (z != -1) cnt[z] += 1;
-}
-int root = 0;
-for (int i=1; i<=n; i++) {
-    if (cnt[i] == 0) {
-        root = i;
     }
-}
-inorder(root, 1);
-int maxdepth = 0;
-for (int i=1; i<=n; i++) {
-    int depth = a[i].depth;
-    int order = a[i].order;
-    if (left[depth] == 0) {
-        left[depth] = order;
-    } else {
-        left[depth] = min(left[depth], order);
+    int root = 0;
+    for (int i=1; i<=n; i++) {
+        if (cnt[i] == 0) {
+            root = i;
+        }
     }
-    right[depth] = max(right[depth], order);
-    maxdepth = max(maxdepth, depth);
-}
-int ans = 0;
-int ans_level = 0;
-for (int i=1; i<=maxdepth; i++) {
-    if (ans < right[i]-left[i]+1) {
-        ans = right[i]-left[i]+1;
-        ans_level = i;
+    inorder(root, 1);
+    int maxdepth = 0;
+    for (int i=1; i<=n; i++) {
+        int depth = a[i].depth;
+        int order = a[i].order;
+        if (left[depth] == 0) {
+            left[depth] = order;
+        } else {
+            left[depth] = min(left[depth], order);
+        }
+        right[depth] = max(right[depth], order);
+        maxdepth = max(maxdepth, depth);
     }
-}
-cout << ans_level << ' ' << ans << '\n';
-return 0;
+    int ans = 0;
+    int ans_level = 0;
+    for (int i=1; i<=maxdepth; i++) {
+        if (ans < right[i]-left[i]+1) {
+            ans = right[i]-left[i]+1;
+            ans_level = i;
+        }
+    }
+    cout << ans_level << ' ' << ans << '\n';
+    return 0;
 }
