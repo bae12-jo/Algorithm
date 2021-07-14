b052899703df7550636540dd1fa527cb3f2a7826
Bae12 <bae12.jo@gmail.com>
2021-07-15 02:15:58 +0900

    210714 브포

--- BOJ/BruteForce/1107.cpp
+++ BOJ/BruteForce/1107.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 using namespace std;
-bool broken[10];
+bool broken[10]; // 버튼이 고장나 있으면 true, 아니면 false
 int possible(int c) {
-    if (c == 0) {
-    if (broken[0]) {
-        return 0;
-    } else {
-        return 1;
+    if (c==0){ // 이동하려는 채널이 0인 경우는 모듈러 연산의 반례가 되므로 예외 처리
+		if(broken[0]){
+			return broken[0] ? 0 : 1;
         }
-    }
-    int len = 0;
+	}
+    int len = 0; // 수의 길이를 확인
     while (c > 0) {
         if (broken[c % 10]) {
         return 0;
@@ -29,6 +27,7 @@ int main() {
         cin >> x;
         broken[x] = true;
     }
+    // 숫자 버튼을 누르지 않는 경우를 초기값으로 설정
     int ans = n - 100;
     if (ans < 0) {
         ans = -ans;
@@ -36,12 +35,12 @@ int main() {
     for (int i = 0; i <= 1000000; i++) {
         int c = i;
         int len = possible(c);
-        if (len > 0) {
+        if (len > 0) { // 0이면 불가능
             int press = c - n;
             if (press < 0) {
                 press = -press;
             }
-            if (ans > len + press) {
+            if (ans > len + press) { // 최소값을 매번 비교
                 ans = len + press;
             }
         }
