c19b1c1ea596ea8235d32c5053464a338b2895b3
bae12-jo <bae12.jo@gmail.com>
2021-08-13 01:26:55 +0900

    210812 Sort

---
+++ BOJ/Sort/10814.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+struct Person{
+    int age;
+    string name;
+    int join_date;
+};
+
+bool compare(Person a, Person b){
+    if (a.name == b.name)
+        return a.join_date < b.join_date;
+    return a.age < b.age;
+}
+
+int main(){
+
+    int num, x, y;
+    cin >> num;
+    vector<Person> vec(num);
+    for(int i=0; i<num; i++){
+        cin >> vec[i].age >> vec[i].name;
+        vec[i].join_date = i;
+    }
+
+    sort(vec.begin(), vec.end(), compare);
+
+    for(int i=0; i<num; i++){
+        cout << vec[i].age << ' ' << vec[i].name << '\n';
+    }
+
+    return 0;
+}
