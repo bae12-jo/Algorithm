0a61b406d60229eab692e4d0af7f8c5b69798724
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-02-26 20:48:35 +0900

    220226 binary tree constructing

--- Leetcode/Medium/construct_tree_from_inorder_and_preorder.java
+++ Leetcode/Medium/construct_tree_from_inorder_and_preorder.java
@@ -1,25 +1,22 @@
 class Solution
 {
-    
-    public static HashMap<Integer,Integer> m=new HashMap<>();
-    public static int preStart = 0;
-    
     public static Node buildTree(int inorder[], int preorder[], int n)
     {
+        HashMap<Integer,Integer> m=new HashMap<>();
         for(int i=0;i<inorder.length;i++){
             m.put(inorder[i],i);
         }
-        Node root=buildTree(preorder, inorder, 0, inorder.length-1);
+        Node root=buildTree(preorder,0,preorder.length-1,inorder,0,inorder.length-1,m);
         return root;
     }
-    static Node buildTree(int[] preorder, int[] inorder, int inStart, int inEnd){
-        if(inStart>inEnd || preStart>preorder.length-1) return null;
+    static Node buildTree(int[] preorder,int preStart,int preEnd,int[] inorder,int inStart,int inEnd,HashMap<Integer,Integer> m){
+        if(preStart>preEnd || inStart>inEnd) return null;
         
-        Node root=new Node(preorder[preStart++]);
+        Node root=new Node(preorder[preStart]);
         int inRoot=m.get(root.data);
-        root.left=buildTree(preorder, inorder, inStart, inRoot-1);
-        root.right=buildTree(preorder, inorder, inRoot+1, inEnd);
-        
+        int numsLeft=inRoot-inStart;
+        root.left=buildTree(preorder,preStart+1,numsLeft+preStart,inorder,inStart,inRoot-1,m);
+        root.right=buildTree(preorder,numsLeft+preStart+1,preEnd,inorder,inRoot+1,inEnd,m);
         return root;
     } 
 }
