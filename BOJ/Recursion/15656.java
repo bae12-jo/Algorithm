961f2b2c3f48854e81866b594313112829194321
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-03-02 17:42:55 +0900

    220302 backtracking

---
+++ BOJ/Recursion/15656.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class BOJ_15656{
+
+	public static int n, m;
+	public static boolean[] visited;
+	public static int[] input;
+	public static int[] arr;
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void dfs(int depth){
+		if(depth==m){
+			for(int val: arr){
+				sb.append(val).append(" ");
+			}
+			sb.append("\n");
+			return ;
+		}
+		
+		for(int i=0; i<n; i++){
+			arr[depth] = input[i];
+			dfs(depth+1);
+		}
+	}
+
+	public static void main(String[] args) throws IOException{
+		
+		/* BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken()); */
+		
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		m = sc.nextInt();
+		
+		input = new int[n];
+		visited = new boolean[n];
+		arr = new int[m];
+		
+		sc.nextLine();
+		
+		for(int i=0; i<n; i++){
+			input[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(input);
+		
+		dfs(0);
+		System.out.println(sb);
+	
+	}
+}
