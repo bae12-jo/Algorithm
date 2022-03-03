2b954b2e838d33e658dcb86b3b8737d9e24363ac
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-03-03 14:33:12 +0900

    220302 graph

--- BOJ/Graph/1388.java
+++ BOJ/Graph/1388.java
@@ -87,3 +87,70 @@ public class BOJ_1388{
 	
 	}
 }
+
+<- 최적화 코드 ->
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+	static int N, M;
+	static char[][] room;
+
+	public static void main(String[] args) throws Exception {
+		input();
+		System.out.println(getWoodsCnt());
+	}
+
+	private static int getWoodsCnt() {
+		int cnt = 0;
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (room[i][j] == '.')
+					continue;
+
+				cnt++;
+				visitWood(i, j, room[i][j]);
+			}
+		}
+
+		return cnt;
+
+	}
+
+	private static void visitWood(int r, int c, char shape) {
+		if (!isIn(r, c) || room[r][c] != shape) {
+			return;
+		}
+
+		room[r][c] = '.';
+		if (shape == '-') {
+			// 모양이 -면 오른쪽으로만 확인하며 같은 방문 체크
+			visitWood(r, c + 1, shape);
+		} else {
+			// 모양이 ㅣ면 아래로맨 확인하며 같은 모양 방문 체크
+			visitWood(r + 1, c, shape);
+		}
+	}
+
+	private static boolean isIn(int r, int c) {
+		return r < N && c < M;
+	}
+
+	private static void input() throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		room = new char[N][];
+		for (int i = 0; i < N; i++) {
+			room[i] = in.readLine().toCharArray();
+		}
+	}
+
+}
