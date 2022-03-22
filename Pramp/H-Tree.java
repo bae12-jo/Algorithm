d8c5fb1d4641d92561abc4c02ab835d8fecc19d8
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-03-22 18:09:44 +0900

    Update H-Tree.java

--- Pramp/H-Tree.java
+++ Pramp/H-Tree.java
@@ -38,7 +38,16 @@ Constraints:
 0 ≤ depth ≤ 10
  */
 
-//SOLUTION:
+
+// SOLUTION
+
+/*
+
+Time Complexity: every call of drawHTree invokes 9 expressions whose time complexity is O(1) and 4 calls of drawHTree until depth (denoted here as D) reaches to 0. Therefore: T(D) = 9 + 4 * T(D-1), where T is the time complexity function and D is the depth of the H-Tree. Now, if we expand T(D-1) recursively all the way to T(0), it’ll be easy to see that T(D) = O(4^D).
+
+Space Complexity: recursive calls add overhead since we store them in the execution stack. The space occupied in the stack will be then O(D), in the worst case scenario. The stack space occupied will be no more than O(D) at any given point since a sibling drawHTree will not be called before the current one being executed returns (i.e. finishes its execution).
+
+*/
 
 
 import java.util.*;
