6fbd1a3ab036734f81c01268777124a8415de6ab
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-03-25 13:07:37 +0900

    HashMap<String, Integer>, getOrDefault

--- Programmers/고득점Kit/해시/완주하지 못한 선수.java
+++ Programmers/고득점Kit/해시/완주하지 못한 선수.java
@@ -1,29 +1,61 @@
+/* Basic Code */
+
+
 import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         
-        HashMap<String, Integer> paticipantList = new HashMap<>();
+        HashMap<String, Integer> participantList = new HashMap<>();
         
         // set <participant, duplication count> hash table
         for(String s: participant){
-            Integer count = paticipantList.get(s);
-            if(count==null) paticipantList.put(s, 1); 
-            else paticipantList.put(s, ++count);
+            Integer count = participantList.get(s);
+            if(count==null) participantList.put(s, 1); 
+            else participantList.put(s, ++count);
         }
         
         // decrease duplication count based on completion list
         for(String s: completion){
             if(paticipantList.containsKey(s)){
-                Integer count = paticipantList.get(s);
-                paticipantList.put(s, --count);
+                Integer count = participantList.get(s);
+                participantList.put(s, --count);
             }
         }
         
         // find out the person with duplication count of 1
-        for(String s: paticipantList.keySet()){
-            int count = paticipantList.get(s);
+        for(String s: participantList.keySet()){
+            int count = participantList.get(s);
+            if(count!=0) answer = s;
+        }
+        
+        // return the name of the person who did not cross the finish line 
+        return answer;
+    }
+}
+
+
+/* getOrDefault 사용 */
+// 궁금한 점: getOrDefault에 ++나 -- 연산자를 바로 사용하면 variable required but value 라는 에러가 뜬다. 왜 일까?
+
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        HashMap<String, Integer> participantList = new HashMap<>();
+        
+        // set <participant, duplication count> hash table
+        for(String s: participant) participantList.put(s, participantList.getOrDefault(s, 0)+1);
+        
+        // decrease duplication count based on completion list
+        for(String s: completion) participantList.put(s, participantList.get(s)-1);
+        
+        // find out the person with duplication count of 1
+        for(String s: participantList.keySet()){
+            int count = participantList.get(s);
             if(count!=0) answer = s;
         }
         
