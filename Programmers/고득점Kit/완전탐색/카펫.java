1f0adb5c5b7719b90067dc387eb1e29a42868d0b
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-03-29 23:57:59 +0900

    Divisor, Matrix

--- Programmers/고득점Kit/완전탐색/카펫.java
+++ Programmers/고득점Kit/완전탐색/카펫.java
@@ -4,13 +4,21 @@ class Solution {
         
         int area = brown+yellow;
         
+        int row = 0, col = 0;
         for(int i=1; i<=area; ++i){
-            int row = i;
-            int col = area/i;
+            
+            // check i is divisor of area
+            if(area%i==0){
+                row = i;
+                col = area/i;
+            }else{
+                continue;
+            }
             
             // row shouldn't be less than col
             if(row>col) continue;
             
+            // if yellow can be made by divisors minus 2
             if((row-2)*(col-2)==yellow){
                 answer[0] = col;
                 answer[1] = row;
