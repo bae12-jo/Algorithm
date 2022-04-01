a84e897b5a61badcd93507218f3d60747d2101c9
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-04-01 17:05:09 +0900

    Update 단어 변환.java

--- Programmers/고득점Kit/DFSBFS/단어 변환.java
+++ Programmers/고득점Kit/DFSBFS/단어 변환.java
@@ -39,3 +39,41 @@ class Solution {
         
     }
 }
+
+// 문자 하나만 다른 단어 찾는 기능을 함수화한 코드
+
+public class WordConversion {
+    int answer;                
+    boolean[] used;               
+    public int solution(String begin, String target, String[] words) {
+        answer = 51; // maximum number of words length (which is given in the problem)
+        used = new boolean[words.length];
+        dfs(begin, target, 0, words);
+        return answer == 51 ? 0 : answer;
+    }
+    
+    public void dfs(String presentWord, String target, int count,String[] words) {
+        if(presentWord.equals(target)) {
+            answer = (answer > count) ? count : answer;
+            return;
+        }
+        
+        for(int i = 0; i < words.length; i++) {  
+            if(!used[i] && check(presentWord, words[i])) {
+                used[i] = true;
+                dfs(words[i],target,count+1, words);
+                used[i] = false;
+            }
+        }
+    }
+    
+    public boolean check(String presentWord, String nextWord) {  
+        int count = 0;
+        for(int i = 0; i < presentWord.length(); i++) {
+            if(presentWord.charAt(i) != nextWord.charAt(i)) {
+                count++;
+            }
+        }
+        return count == 1 ? true : false;
+    }
+}
