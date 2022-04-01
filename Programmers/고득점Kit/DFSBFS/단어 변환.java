15148c4e8d72cd3b37dbe866f2af4543c8a3514b
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-04-01 17:02:19 +0900

    DFS를 이용한 최단 경로 찾기

--- Programmers/고득점Kit/DFSBFS/단어 변환.java
+++ Programmers/고득점Kit/DFSBFS/단어 변환.java
@@ -1,25 +1,22 @@
 // 문제 조건
 // (1) 출발점이 begin 단어로 고정되어 있으며 conntected components에 대해 고민할 필요가 없다
-// (2) begin에서 target으로 가기까지 단어 배열에 따라 경우의 수가 여러개 나올 수 있지만 count 자체는 동일하다
 
 class Solution {
     
     boolean[] visited;
-    int answer = 0;
+    int answer = 51;
     
     public int solution(String begin, String target, String[] words) {
         
         visited = new boolean[words.length];
-        
         DFS(words, begin, target, 0);
-        
-        return answer;
+        return answer == 51 ? 0 : answer;
     }
     
     public void DFS(String[] words, String begin, String target, int count){
         
         if(begin.equals(target)){
-            answer = ++count;
+            answer = (answer > count) ? count : answer;
             return;
         }
         
@@ -34,7 +31,7 @@ class Solution {
             
             if(sameChar==begin.length()-1){
                 visited[i]=true;
-                DFS(words, words[i], target, count++);
+                DFS(words, words[i], target, count+1);
                 visited[i]=false;
             }
             
