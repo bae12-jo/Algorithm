10a20940eaba23321c445f98b23b41cfbfa860a0
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-04-07 01:36:19 +0900

    bucket sort

---
+++ Leetcode/Medium/H-index.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        
+        int[] bucket = new int[citations.length+1];
+        
+        for(Integer c: citations){
+            if(c>=citations.length) bucket[citations.length]++;
+            else bucket[c]++;
+        }
+        
+        int count = 0;
+        for(int i=citations.length; i>0; i--){
+            count += bucket[i];
+            if(count >= i) return i;
+            
+        }
+        
+        return 0;
+        
+    }
+}
