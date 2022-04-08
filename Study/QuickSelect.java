330bc2980770501086d2092037626a4611f56394
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-04-08 21:39:05 +0900

    QuickSelect with Medians of Medians

--- Study/QuickSelect.java
+++ Study/QuickSelect.java
@@ -1,66 +1,112 @@
 import java.util.*;
 
-/*
-* Kth smallest element in unsorted array
-*/
+//User function Template for Java
 
-class QuickSelect{
-	
-	// find kth smallest -- by comparing K and returned pivot after partioning
-	
-	public static int getKthSmallest(int[] arr, int l, int r, int k){
-	
-		// to avoid k is out of traversal range
-		if(k > 0 && k <= r-l+1){ // there is no zero in ordinal number
-			
-			int newPivot = getRandomPivot(arr, l, r);
-			if(newPivot-1==k-1) return arr[newPivot]; // if newPivot is same as k
-			else if(newPivot-1<k-1) return getKthSmallest(arr, newPivot+1, r, k+l-newPivot-1); // if newPivot is smaller than k 
-			return getKthSmallest(arr, 1, newPivot-1, k); // if newPivot is bigger than k
-		}
-		
-		return Integer.MAX_VALUE;
-	
-	}
-	
-	// get a ramdom pivot
-	public static int getRandomPivot(int[] arr, int l, int r){
-		int pivot = (int)(Math.random()*(r-l)); // 0부터 탐색범위-1 인덱스까지 랜덤하게 뽑는 함수 (인덱스라서 r-l+1이 아님)
-		swap(arr, l+pivot, r); // move pivot value to rightmost index for partioning
-		return partitioning(arr, l, r);
-		
-	}
-	
-	// partioning based on a selected random pivot
-	public static int partitioning(int[] arr, int l, int r){
-		int pivotValue = arr[r], bigger = l;
-		for(int i = l; i < r; ++i){
-			if(arr[i]<=pivotValue){
-				swap(arr, i, bigger); // swap element less than pivot with first bigger one
-				bigger++; // update index of fisrt element bigger than pivot value
-			}
-		}
-		swap(arr, bigger, r); // reposition to the position where the pivot value should be
-		return bigger; // return position of pivot value
-	}
-	
-	// swap -- utility method - parameter (array, left index, right index)
-	public static void swap(int[] arr, int i, int j){
-		int tmp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = tmp;
-	}
-	
+class QuickSelectWithMOM{
+    public static int kthSmallest(int[] arr, int l, int r, int k) 
+    {   
+        // If k is smaller than 
+    // number of elements in array
+    if (k > 0 && k <= r - l + 1)
+    {
+        int n = r - l + 1 ; // Number of elements in arr[l..r]
+
+        // Divide arr[] in groups of size 5, 
+        // calculate median of every group
+        //  and store it in median[] array.
+        int i;
+        
+         // There will be floor((n+4)/5) groups;
+        int []median = new int[(n + 4) / 5]; // 올림 효과, 나머지가 생기지 않도록 방지
+        for (i = 0; i < n/5; i++)
+            median[i] = findMedian(arr, l+i*5, l+i*5+5);
+            
+        // For last group with less than 5 elements
+        if (i*5 < n) 
+        {
+            median[i] = findMedian(arr, l+i*5, l+i*5+n%5); 
+            i++;
+        } 
+
+        // Find median of all medians using recursive call.
+        // If median[] has only one element, then no need
+        // of recursive call
+        int medOfMed = (i == 1)? median[i - 1]:
+                                kthSmallest(median, 0, i - 1, i / 2);
+
+        // Partition the array around a random element and
+        // get position of pivot element in sorted array
+        int pos = partition(arr, l, r, medOfMed);
+
+        // If position is same as k
+        if (pos-l == k - 1)
+            return arr[pos];
+        if (pos-l > k - 1) // If position is more, recur for left
+            return kthSmallest(arr, l, pos - 1, k);
+
+        // Else recur for right subarray
+        return kthSmallest(arr, pos + 1, r, k - pos + l - 1);
+    }
+
+    // If k is more than number of elements in array
+    return Integer.MAX_VALUE;
+    }
+    
+    public static int[] swap(int []arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        return arr;
+    }
+    
+    // It searches for x in arr[l..r], and 
+    // partitions the array around x.
+    public static int partition(int arr[], int l,
+                            int r, int x)
+    {
+        // Search for x in arr[l..r] and move it to end
+        int i;
+        for (i = l; i < r; i++)
+            if (arr[i] == x)
+            break;
+        swap(arr, i, r);
+    
+        // Standard partition algorithm
+        i = l;
+        for (int j = l; j <= r - 1; j++)
+        {
+            if (arr[j] <= x)
+            {
+                swap(arr, i, j);
+                i++;
+            }
+        }
+        swap(arr, i, r);
+        return i;
+    }
+    
+    // int partition(int arr[], int l, int r, int k);
+
+    // A simple function to find median of arr[]. This is called
+    // only for an array of size 5 in this program.
+    public static int findMedian(int arr[], int i,int n)
+    {
+            Arrays.sort(arr, i, n);
+            return arr[i+(n-i)/2];                    // sort the array and return middle element
+    }
 	
 	public static void main(String[] args){
 		
-		int[] arr = {12, 3, 5, 7, 4 ,19, 26};
+		int[] arr = {7, 10, 4, 3, 20, 15};
 		int k = 3;
 		
 		// 메소드를 바로 사용할거면 전부 public static으로 선언해줘야 한다
 		// 접근자를 public으로 하지 않으면 인스턴스를 생성해서 사용해야 한다.
-		System.out.print(getKthSmallest(arr, 0, arr.length-1, k));
+		// kthSmallest = ob = new kthSmallest();
+		// ob.kthSmallest(arr, 0, n-1, k); // main과 다른 클래스에 선언된 경우
+		System.out.print(kthSmallest(arr, 0, arr.length-1, k));
 	
 	}
-	
+    
 }
