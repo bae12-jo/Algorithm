239dbbe9bd8a3b8a1fad91da9438a773babc8807
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-04-11 21:44:37 +0900

    quick select

---
+++ Leetcode/Medium/kth largest element in an array.java
@@ -0,0 +1,58 @@
+class Solution {
+    
+    public static int kthSmallest(int arr[], int l, int r, int k)
+    {
+        if (k > 0 && k <= r - l + 1)
+        {
+            int pos = randomPartition(arr, l, r);
+ 
+            if (pos-l == k-1)
+                return arr[pos];
+ 
+            if (pos-l > k-1)
+                return kthSmallest(arr, l, pos-1, k);
+ 
+            return kthSmallest(arr, pos+1, r, k-pos+l-1);
+        }
+ 
+        return Integer.MAX_VALUE;
+    }
+ 
+    public static void swap(int arr[], int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+ 
+
+    public static int partition(int arr[], int l, int r)
+    {
+        int x = arr[r], i = l;
+        for (int j = l; j < r; j++)
+        {
+            if (arr[j] <= x)
+            {
+                swap(arr, i, j);
+                i++;
+            }
+        }
+        swap(arr, i, r);
+        return i;
+    }
+ 
+    public static int randomPartition(int arr[], int l, int r)
+    {
+        int n = r - l + 1;
+        int pivot = (int)(Math.random() * (n - 1));
+        swap(arr, l + pivot, r);
+        return partition(arr, l, r);
+    }
+    
+    public int findKthLargest(int[] nums, int k) {
+        
+        k = nums.length-k+1;
+        return kthSmallest(nums, 0, nums.length-1, k);
+        
+    }
+}
