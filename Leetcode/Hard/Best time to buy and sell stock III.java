3460438f7b09f5d1c82417dae79ebc09d63344aa
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-04-13 20:47:18 +0900

    DP

--- Leetcode/Hard/Best time to buy and sell stock III.java
+++ Leetcode/Hard/Best time to buy and sell stock III.java
@@ -1,4 +1,60 @@
+/* 1 simluation */
+// Time O(n) Space O(1)
+
+// 재투자 지출을 -로 보는 관점
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int firstTransactionCost = prices[0];
+        int profitFromFirstTransaction = 0;
+        
+        int secondTransactionCost = -prices[0];
+        int profitFromTwoTransactions = 0;
+        
+        for(int currPrice: prices){
+            firstTransactionCost = Math.min(firstTransactionCost, currPrice);
+            profitFromFirstTransaction = Math.max(profitFromFirstTransaction, currPrice-firstTransactionCost);
+            
+            secondTransactionCost = Math.max(secondTransactionCost, profitFromFirstTransaction-currPrice);
+            profitFromTwoTransactions = Math.max(profitFromTwoTransactions, secondTransactionCost + currPrice);
+        }
+        
+        return profitFromTwoTransactions;
+        
+    }
+}
+
+// 재투자 지출을 +로 보는 관점
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int n = prices.length;
+        if(n<=1) return 0;
+        
+        int t1Cost = Integer.MAX_VALUE, 
+        t2Cost = Integer.MAX_VALUE;
+        int t1Profit = 0,
+        t2Profit = 0;
+
+        for (int price : prices) {
+            // the maximum profit if only one transaction is allowed
+            t1Cost = Math.min(t1Cost, price);
+            t1Profit = Math.max(t1Profit, price - t1Cost);
+            System.out.println(t1Cost + " " + t1Profit);
+            // reinvest the gained profit in the second transaction
+            t2Cost = Math.min(t2Cost, price - t1Profit);
+            t2Profit = Math.max(t2Profit, price - t2Cost);
+            System.out.println(t2Cost + " " + t2Profit);
+        }        
+        
+        
+        return t2Profit;
+        
+    }
+}
+
 /* 2 array DP */
+// Time O(n) Space O(n)
 
 class Solution {
     public int maxProfit(int[] prices) {
