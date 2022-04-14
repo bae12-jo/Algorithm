af3850cff72769e3e6ffcd417bd2fd5e2fd39f0f
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-04-14 22:45:01 +0900

    Integer.toBinaryString

---
+++ Programmers/카카오기출/[Level1] 비밀지도.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        
+        int[] map = new int[n];
+        String[] answer = new String[n];
+        for(int i=0; i<n; ++i){
+            String result = Integer.toBinaryString(arr1[i] | arr2[i]);
+            while(result.length()!=n){
+                result = '0' + result;
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int j=0; j<n; ++j){
+                if(result.charAt(j)=='0') sb.append(" ");
+                else sb.append("#");
+            }
+            answer[i] = sb.toString();
+        }        
+        return answer;
+    }
+}
