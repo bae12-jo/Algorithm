d9feea6af4a160eb757796b83a7743dacef0c02d
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-04-19 18:53:45 +0900

    dp bottom up w/ tabluation top down w/ memoizaion

--- Leetcode/Easy/fibonacci number.java
+++ Leetcode/Easy/fibonacci number.java
@@ -51,7 +51,27 @@ class Solution {
     }
 }
 
-/* (4)
-*
-*
+/* (4) iterative bottom-up
+* Time: O(n)
+* Space: O(1)
 */
+
+class Solution {
+    public int fib(int n) {
+        
+        if(n<=1) return n;
+        
+        int curr = 0;
+        int prev1 = 1; // n-1
+        int prev2 = 0; // n-2
+        
+        for(int i=2; i<=n; ++i){
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        
+        return curr;
+        
+    }
+}
