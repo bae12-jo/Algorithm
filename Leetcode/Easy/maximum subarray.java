4215d7a2b59734540db9fe0c6764d945fec26e2d
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-04-21 03:20:37 +0900

    Update maximum subarray.java

--- Leetcode/Easy/maximum subarray.java
+++ Leetcode/Easy/maximum subarray.java
@@ -24,6 +24,22 @@ class Solution {
 }
 
 /* (2) Divide and Conquer
-*
-*
+* Time: O(n+m)
+* Space O(1)
 */
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int row = matrix.length-1;
+        int col = 0;
+        
+        while(row>=0 && col<=matrix[0].length-1){
+            if(matrix[row][col]<target) col++;
+            else if(matrix[row][col]>target) row--;
+            else return true;
+        }
+        
+        return false;
+        
+    }
+}
