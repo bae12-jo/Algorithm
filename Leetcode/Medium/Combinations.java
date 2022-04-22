b1f148c553901226bb7a60583306e2bf73266698
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-04-22 16:14:51 +0900

    n과 m(2)

--- Leetcode/Medium/Combinations.java
+++ Leetcode/Medium/Combinations.java
@@ -30,3 +30,47 @@ class Solution {
         
     }
 }
+
+
+/* 글로벌 필드 없는 버전 */
+
+class Solution {
+    
+    public void getCombine(int n, int k, int start, List<List<Integer>> res, List<Integer> list){
+        
+        // base case
+        if(list.size()==k){
+            res.add(new ArrayList(list));
+            return;
+        }
+        
+        for(int i=start; i<=n+1-(k-list.size()); ++i){ // n 대신 동적 범위를 주는 것이 성능 향상에 큰 도움이 됨
+            list.add(i);
+            getCombine(n, k, i+1, res, list);
+            list.remove(list.size()-1);
+        }
+        
+    }
+    
+    public List<List<Integer>> combine(int n, int k) {
+        
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        
+        getCombine(n, k, 1, res, new ArrayList());
+        
+        return res;
+        
+    }
+}
+    
+    public List<List<Integer>> combine(int n, int k) {
+        
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<Integer> list = new ArrayList<Integer>();
+        
+        getCombine(n, k, 1, res, list);
+        
+        return res;
+        
+    }
+}
