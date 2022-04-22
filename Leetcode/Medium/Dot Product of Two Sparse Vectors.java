72f878474404e95d6d114abe732af3e531d8840e
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-04-22 23:27:48 +0900

    hashmap, design

---
+++ Leetcode/Medium/Dot Product of Two Sparse Vectors.java
@@ -0,0 +1,60 @@
+// O(nm)
+
+class SparseVector {
+    private int[] array;
+    
+    SparseVector(int[] nums) {
+        array = nums;
+    }
+    
+	// Return the dotProduct of two sparse vectors
+    public int dotProduct(SparseVector vec) {
+        int result = 0;
+        
+        for (int i = 0; i < array.length; i++) {
+            result += array[i] * vec.array[i];
+        }
+        
+        return result;
+    }
+}
+
+// Your SparseVector object will be instantiated and called as such:
+// SparseVector v1 = new SparseVector(nums1);
+// SparseVector v2 = new SparseVector(nums2);
+// int ans = v1.dotProduct(v2);
+
+// O(nm) with less memory
+
+class SparseVector {
+    
+    HashMap<Integer, Integer> map = new HashMap<>();
+    
+    SparseVector(int[] nums) {
+        
+        for(int i=0; i<nums.length; ++i){
+            if(nums[i]!=0) map.put(i, nums[i]);
+        }
+        
+    }
+    
+	// Return the dotProduct of two sparse vectors
+    public int dotProduct(SparseVector vec) {
+        
+        int result = 0;
+        
+        for(Integer i: this.map.keySet()){
+            if(vec.map.containsKey(i)){
+                result += this.map.get(i) * vec.map.get(i);
+            }
+        }
+        
+        return result;
+        
+    }
+}
+
+// Your SparseVector object will be instantiated and called as such:
+// SparseVector v1 = new SparseVector(nums1);
+// SparseVector v2 = new SparseVector(nums2);
+// int ans = v1.dotProduct(v2);
