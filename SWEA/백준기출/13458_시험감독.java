c63ffe2066131e627ef99bb71ceb17d7690579fd
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-04-23 00:12:01 +0900

    arithmetic

---
+++ SWEA/백준기출/13458_시험감독.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+
+	public static BufferedReader br;
+	public static StringTokenizer st, st2;
+
+	public static void main(String[] args) throws IOException{
+		
+		br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		long count = 0;
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		
+		st2 = new StringTokenizer(br.readLine(), " ");
+		
+		int b = Integer.parseInt(st2.nextToken());
+		int c = Integer.parseInt(st2.nextToken());
+		
+		
+		for(int i=0; i<n; i++){
+			int tmp = Integer.parseInt(st.nextToken())-b;
+			count++;
+			if(tmp>0) count+=(tmp+c-1)/c;
+		}
+		
+		System.out.println(count);
+	
+	}
+}
