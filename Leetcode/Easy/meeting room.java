ec8ba483ea811f6886e45863b54ad7840bccbd8a
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-04-25 17:54:23 +0900

    array, sorting

---
+++ Leetcode/Easy/meeting room.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        
+        Arrays.sort(intervals, (a, b)->Integer.compare(a[1], b[1]));
+        
+        for(int i=1; i<intervals.length; ++i){
+            if(intervals[i-1][1]>intervals[i][0]) return false;
+  
+        }
+        
+        return true;
+        
+    }
+}
