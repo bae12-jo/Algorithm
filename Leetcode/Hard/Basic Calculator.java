7c72528f5ad0e6195ce064b8780d6b47e4324273
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-05-31 19:28:55 +0900

    Stack or Recursion

--- Leetcode/Hard/Basic Calculator.java
+++ Leetcode/Hard/Basic Calculator.java
@@ -1,3 +1,5 @@
+// stack
+
 class Solution {
     public int calculate(String s) {
         
@@ -34,5 +36,54 @@ class Solution {
         }
         
         return tmpRes + (sign*currNum);
+    }
+}
+
+
+// recursion
+
+
+class Solution {
+    
+    public int calculate(String s) {
+        
+        // init queue
+        Queue<Character> tokens = new ArrayDeque<Character>();
+        for(char c: s.toCharArray()) if(c!=' ') tokens.offer(c);
+        tokens.offer('+'); // to calculate last tmp sum with prev one
+        
+        return calculate(tokens);
+    }
+    
+    private int calculate(Queue<Character> tokens){
+        
+        char preOp = '+'; // base operators is +
+        int num = 0, sum = 0, prev = 0; // current number, on-going result, lase number which is not calculated yet
+        
+        while(!tokens.isEmpty()){
+            
+            char c = tokens.poll();
+            if(Character.isDigit(c)) num = num*10 + c-'0';
+            else if(c=='(') num = calculate(tokens);
+            else{
+                switch(preOp){
+                    case '+':
+                        sum += prev;
+                        prev = num; // signed num
+                        break;
+                    case '-':
+                        sum += prev;
+                        prev = -num; // signed num
+                        break;
+                }
+                
+                if(c==')') break; // go prev func
+                preOp = c; // update operator
+                num = 0; // init current number
+            }   
+        }
+        
+        return sum + prev;
+        
     }
 }
