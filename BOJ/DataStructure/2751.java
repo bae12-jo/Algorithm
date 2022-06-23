940fd5a5806a73c8cbfb1fdedb580659dd26fb55
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-06-23 21:15:25 +0900

    priorityQueue, array sorting

--- BOJ/DataStructure/2751.java
+++ BOJ/DataStructure/2751.java
@@ -1,3 +1,5 @@
+// PriorityQueue
+
 import java.io.*;
 import java.util.*;
 
@@ -19,6 +21,41 @@ public class Main{
 		
 		for(int testcase=0; testcase<T; testcase++){
 			bw.write(String.valueOf(pq.poll())+'\n');
+		}		
+
+		br.close();
+		bw.flush();
+		bw.close();
+		
+	}
+
+}
+
+// array sort
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_2751{
+	
+	
+	public static void main(String[] args) throws IOException{
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// init T
+		int T = Integer.parseInt(br.readLine());
+		int arr[] = new int[T];
+		
+		for(int testcase=0; testcase<T; testcase++){
+			arr[testcase] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+		
+		for(int testcase=0; testcase<T; testcase++){
+			bw.write(String.valueOf(arr[testcase])+'\n');
 		}		
 
 		br.close();
